32d828897c16f427496cc1ab2f1ca136b8e033c9
unknown <wonjiprog@gmail.com>
2022-12-13 16:48:06 +0900

    [ADD]Chatbot project files

---
+++ config/DatabaseConfig.py
@@ -0,0 +1,9 @@
+DB_HOST = "127.0.0.1"
+DB_PORT = 3307
+DB_USER = "root"
+DB_PASSWORD = "1234"
+DB_NAME = "homestead"
+
+def DatabaseConfig():
+    global DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
+
---
+++ test/test11.py
@@ -0,0 +1,27 @@
+from utils.Preprocess import Preprocess
+
+sent = "내일 오전 10시에 탕수육 주문하고 싶어"
+
+# 전처리 객체 생성
+p = Preprocess(userdic = '../utils/user_dic.tsv')
+
+# 형태소 분석기 실행
+pos = p.pos(sent)
+
+# 품사 태그와 같이 키워드 출력
+ret = p.get_keywords(pos, without_tag=False)
+print(ret)
+
+# 품사 태그 없이 키워드 출력
+keywords = p.get_keywords(pos, without_tag=True)
+print(keywords)
+
+# w2i = p.get_wordidx_sequence(keywords)
+# sequences = [w2i]
+#
+# MAX_SEQ_LEN = 15    # 임베딩 벡터 크기c
+# padded_seqs = preprocessing.sequence.pad_sequences(sequences, maxlen=MAX_SEQ_LEN, padding='post')
+#
+# print(keywords)
+# print(sequences)
+# print(padded_seqs)
